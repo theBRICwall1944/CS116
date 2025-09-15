dca155be1d7cfa0d8ece2409e8ff9b4e8a868225
theBRICwall1944 <joseph.brickhousejr2@gmail.com>
2025-09-15 12:14:06 -0400

    Implement sentence splitting function and integrate into main

--- Hw1Prob1.cpp
+++ Hw1Prob1.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
 using namespace std;
 
-int main(){
+vector<string> split_sentence(string sentence){
+
+    stringstream ss(sentence);
+
+    string word;
     
-    for (int i = 0; i <25; ++i){
-        
+    vector<string> words;
+
+    while (ss >> word){
+
+        words.push_back(word);
+
     }
+
+    return words;
+}
+int main(){
+    string sentence;
+
+    cout << "please enter a sentence" << endl;
+
+    cin >> sentence;
+
+    vector<string> words = split_sentence(sentence);
+
+    for (string w : words){
+
+        cout << w << endl;
+    }
+
+    return 0;
 }
